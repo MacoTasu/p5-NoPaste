014dce18cc943803fcc1a35cf3435621a77fc293
MacoTasu <maco.tasu@gmail.com>
2015-11-19 03:09:54 +0900

    投稿内容の文字数をチェックするタイプを追加

--- lib/NoPaste/Type.pm
+++ lib/NoPaste/Type.pm
@@ -6,14 +6,42 @@ use utf8;
 use Mouse::Util::TypeConstraints;
 
 subtype 'UInt'
-    => as "Int"
+    => as 'Int'
     => where {
         my $value = shift;
-        return if not defined $value;
         return if $value < 0;
         return 1;
     }
     => message {
         my $value = shift;
-        "This number ($value) is not less than zero!"
+        return "This number ($value) is not less than zero!";
+    };
+
+subtype 'EntryTitleWidth'
+    => as 'Maybe[Str]'
+    => where {
+        my $value = shift;
+        return 1 if not defined $value;
+
+        my $size = length($value);
+        return 0 if 120 < $size;
+        return 1;
+    }
+    => message {
+        my $value = shift;
+        return "Number of characters in the title ($value) is not more than 120!";
+    };
+
+subtype 'EntryBodyWidth'
+    => as 'Str'
+    => where {
+        my $value = shift;
+        my $size = length($value);
+        return 0 if $size <= 0;
+        return 0 if 10000 < $size;
+        return 1;
+    }
+    => message {
+        my $value = shift;
+        return "Number of characters in the body ($value), caracters is 1 to 10,000!";
     };
--- t/01_type.t
+++ t/01_type.t
@@ -6,10 +6,30 @@ use_ok "NoPaste::Type";
 subtest "UInt" => sub {
     my $type = find_type_constraint("UInt");
 
-    ok $type->check(1), 'is valid: 1';
-    ok $type->check(0), 'is valid: 0';
+    ok $type->check(1), 'valid: 1';
+    ok $type->check(0), 'valid: 0';
     ok !$type->check(-1), 'invalid: -1';
-    ok !$type->check(undef), 'is valid: undef';
+    ok !$type->check(undef), 'valid: undef';
+};
+
+subtest "EntryTitleWidth" => sub {
+    my $type = find_type_constraint("EntryTitleWidth");
+
+    ok $type->check(undef), 'valid: undef';
+    ok $type->check(''), 'valid: ""';
+    ok $type->check('a'), 'valid: a';
+    ok $type->check('a'x120), 'valid: ax120';
+    ok !$type->check('a'x121), 'inalid: ax121';
+};
+
+subtest "EntryBodyWidth" => sub {
+    my $type = find_type_constraint("EntryBodyWidth");
+
+    ok $type->check('a'), 'valid: a';
+    ok $type->check('a'x10000), 'valid: ax10000';
+    ok !$type->check('a'x10001), 'inalid: ax10001';
+    ok !$type->check(undef), 'invalid: undef';
+    ok !$type->check(''), 'valid: ""';
 };
 
 done_testing;
